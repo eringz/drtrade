afe355bbceb9814f9cd45eb77819c753c06bcc50
ASTA <ron.garcia.santos@gmail.com>
2026-09-08 20:38:08 +0800

    first commit

---
+++ src/main/java/com/drtrade/DrtradeApplication.java
@@ -0,0 +1,13 @@
+package com.drtrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrtradeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrtradeApplication.class, args);
+    }
+
+}
